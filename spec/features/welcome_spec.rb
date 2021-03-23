003c633bd242457fd0948dbb2d2238511fb2d6f0
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-23 16:41:16 -0600

    Add in blurb test
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>
    Co-authored-by: Wil McCauley <64031242+Wil-McC@users.noreply.github.com>

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe 'welcome page' do
     visit root_path
 
     expect(page).to have_content('Welcome')
+    expect(page).to have_selector('#blurb')
   end
-  xit "displays login and register links" do
 
+  it 'displays login and register links' do
+    visit root_path
+
+    expect(page).to have_link('Register')
+    expect(page).to have_link('Log In')
   end
 end
