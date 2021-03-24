2716c387a652021a1fde08a8e15fe2e85e26f9ab
Wil McCauley <wmccauley724@gmail.com>
2021-03-23 18:40:46 -0600

    Add create method with strong params
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.new(user)
+    if new_user.save
+      # refac?
+      session[:user_id] = new_user.id
+      redirect_to dashboard_index_path
+    else
+      flash[:message] = "Invalid Information Entered"
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
