03d732346578b63fef62dfc95593fc68dba64f4d
Wil McCauley <wmccauley724@gmail.com>
2021-03-23 18:44:38 -0600

    Add routes for user controller actions and dashboard route
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :users, only: [:new, :create]
+  resources :dashboard, only: [:index]
   resources :sessions, only: [:new, :create]
 end
