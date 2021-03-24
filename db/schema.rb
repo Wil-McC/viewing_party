f7ae2a0e2341734e26d532fd11801439178c9d4b
Wil McCauley <wmccauley724@gmail.com>
2021-03-24 17:02:03 -0600

    Generate migration for invitees table with user and party references, run migration
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_225330) do
+ActiveRecord::Schema.define(version: 2021_03_24_230037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_03_24_225330) do
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "invitees", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["party_id"], name: "index_invitees_on_party_id"
+    t.index ["user_id"], name: "index_invitees_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -52,6 +61,8 @@ ActiveRecord::Schema.define(version: 2021_03_24_225330) do
 
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
+  add_foreign_key "invitees", "parties"
+  add_foreign_key "invitees", "users"
   add_foreign_key "parties", "movies"
   add_foreign_key "parties", "users"
 end
