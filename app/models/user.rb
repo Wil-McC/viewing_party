7cb0d43e2c7b9464284437e4a2e031749d4ede3b
Wil McCauley <wmccauley724@gmail.com>
2021-03-24 17:52:03 -0600

    Add relationship and validation tests to reflect all table relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :invitees
   has_secure_password
   validates :email, uniqueness: true, presence: true
 end
