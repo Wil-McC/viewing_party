54d43f52be5ed65bb8e8990503418ffd12983ef7
Wil McCauley <wmccauley724@gmail.com>
2021-03-24 17:53:36 -0600

    Add relationship tests for friendships and invitees

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
   end
-  # relationships
+  describe 'relationships' do
+    it { should have_many :friendships }
+    it { should have_many(:friends).through(:friendships) }
+    it { should have_many :invitees }
+  end
 end
