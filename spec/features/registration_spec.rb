aa99d835a79912fae8cf683a392ad5e896d47a13
Wil McCauley <wmccauley724@gmail.com>
2021-03-23 18:48:59 -0600

    Add tests for registration submission and sad path
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -12,11 +12,33 @@ RSpec.describe 'registration form' do
   it 'submits and creates on submit' do
     visit new_user_path
 
-    save_and_open_page
-    fill_in 'email', with: 'beeps@beep.org'
-    fill_in 'password', with: 'secretbeeps'
-
+    fill_in 'user[email]', with: 'beeps@beep.org'
+    fill_in 'user[password]', with: 'secretbeeps'
 
     click_on 'Register'
+
+    expect(current_path).to eq(dashboard_index_path)
+    expect(page).to have_content("Welcome beeps@beep.org!")
+  end
+  it "downcases email on creation" do
+    visit new_user_path
+
+    fill_in 'user[email]', with: 'beePs@BEep.Org'
+    fill_in 'user[password]', with: 'secretbeeps'
+
+    click_on 'Register'
+
+    expect(current_path).to eq(dashboard_index_path)
+    expect(page).to have_content("Welcome beeps@beep.org!")
+  end
+  it "doesn't redirect and displays sad flash on incomplete form" do
+    visit new_user_path
+
+    fill_in 'user[email]', with: 'beePs@BEep.Org'
+
+    click_on 'Register'
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content('Invalid Information Entered')
   end
 end
