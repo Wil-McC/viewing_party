e1082d0e20c23fb186b672bfc16377bfb08ab3bf
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-25 11:23:53 -0600

    Add alias friend for user.
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- spec/support/user.rb
+++ spec/support/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:friend] do
     email { Faker::Internet.safe_email }
-    password_digest { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3) }
-  end 
+    password { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3) }
+  end
 end
