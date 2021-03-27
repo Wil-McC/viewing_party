1dbfeae719c816e3f25ec22a49f2642e6de83e91
Alex Schwartz <aschwartz@detaso.com>
2021-03-27 17:04:50 -0500

    Add find by email method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
   has_many :invitees
   has_many :parties
-  
+
   has_secure_password
   validates :email, uniqueness: true, presence: true
+
+  def self.by_email(email)
+    User.where(email: email).first
+  end
 end
