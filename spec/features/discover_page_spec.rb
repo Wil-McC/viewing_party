3b5c012723ebd615e951157a8d2bcdae74f9a954
Wil McCauley <wmccauley724@gmail.com>
2021-03-28 16:23:28 -0600

    Add happy path testing for discover page queries
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>

--- spec/features/discover_page_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'the discover page' do
-  describe 'sad path' do
-    it 'redirects if user is not logged in' do
-    end
-  end
-end
--- spec/features/discover_spec.rb
+++ spec/features/discover_spec.rb
@@ -2,12 +2,71 @@ require 'rails_helper'
 
 RSpec.describe 'Discover page' do
   describe 'as an authenticated user' do
+    before :each do
+      @user = create(:user)
+      perform_login(@user)
+    end
+    it "shows discover page with welcome message" do
+      visit discover_path
 
+      expect(page).to have_content("Welcome #{@user.email}")
+    end
+    it "has a top rated button that populates results on click" do
+      visit discover_path
+
+      VCR.use_cassette('movies') do
+        click_on 'Top Rated'
+
+        expect(current_path).to eq(movies_path)
+
+        within('#results') do
+          expect(page).to have_selector('p', count: 40)
+          expect(page).to have_link('The Green Mile - 8.5')
+          expect(page).to have_link('The Godfather - 8.7')
+          expect(page).to have_link('City of God - 8.4')
+        end
+      end
+    end
+    it "has a search field that returns max 40 matching results on submit" do
+      VCR.use_cassette('rambo_search') do
+        visit discover_path
+
+        fill_in :query, with: 'rambo'
+        click_button 'Search'
+
+        within('#results') do
+          expect(current_path).to eq(movies_path)
+          expect(page).to have_selector('p', count: 40)
+        end
+      end
+    end
+  end
+
+  describe 'sad paths' do
+    before :each do
+      @user = create(:user)
+      perform_login(@user)
+    end
+    xit "shows flash message when top 40 call fails - 404" do
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MDB_KEY']}&page=1").
+         with(
+           headers: {
+             "api_key": ENV['MDB_KEY']
+           }).
+         to_return(status: 404, body: "", headers: {})
+
+      visit discover_path
+
+      click_on 'Top Rated'
+
+      expect(current_path).to eq(discover_path)
+      expect(page).to have_content('Error loading results')
+    end
   end
 
   describe 'as a non-authenticated user' do
     it 'redirects to homepage with a flash message' do
-      visit dashboard_path
+      visit discover_path
 
       expect(current_path).to eq root_path
       expect(page).to have_content('You must be logged in to access this section')
