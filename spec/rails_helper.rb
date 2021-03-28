27c68034e476dd7d62ccdccfa1b6a8a96005bbee
Wil McCauley <wmccauley724@gmail.com>
2021-03-28 13:48:31 -0600

    Uncomment webmock and VCR config lines in helpers
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,8 +69,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# VCR.configure do |config|
-  # config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  # config.hook_into :webmock
-  # config.filter_sensitive_data('SECURED') { ENV['MDB_KEY'] }
-# end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('SECURED') { ENV['MDB_KEY'] }
+end
