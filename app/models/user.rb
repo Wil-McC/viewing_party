5c26189b859b27ee10f555fcf884c9ccc189f75f
Alex Schwartz <aschwartz@detaso.com>
2021-03-28 18:43:01 -0500

    Add new viewing party associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :friendships
-  has_many :invitees
+  # has_many :invitees, as: :invitations
   has_many :parties
+  has_many :invitations, foreign_key: 'user_id', class_name: 'Invitee'
+  has_many :invited_to_parties, through: :invitations, source: :party
 
   has_secure_password
   validates :email, uniqueness: true, presence: true
@@ -11,7 +13,7 @@ class User < ApplicationRecord
     User.where(email: email).first
   end
 
-  def viewing_parties_invovling_me
-    hosting = parties
+  def viewing_parties_involving_me
+    parties.concat(invited_to_parties)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe User, type: :model do
     it { should have_many :friendships }
     it { should have_many(:friends).through(:friendships) }
     it { should have_many :parties }
-    it { should have_many :invitees }
+    it { should have_many :invitations }
+    it { should have_many(:invited_to_parties).through(:invitations) }
   end
 
   describe 'class methods' do
@@ -29,25 +30,39 @@ RSpec.describe User, type: :model do
 
   describe 'instance methods' do
     describe '#viewing_parties_involving_me' do
-      it 'includes parties I am hosting' do
+      it 'shows hosted and invited-to parties together' do
+        user = create(:user)
+        party_1 = create(:party, user: user)
+        party_2 = create(:party, user: user)
+        party_3 = create(:party)
+        party_4 = create(:party)
+        invitation_1 = create(:invitee, party: party_3, user: user)
+        invitation_2 = create(:invitee, party: party_4, user: user)
+
+        expect(user.viewing_parties_involving_me).to eq([party_1, party_2, party_3, party_4])
+      end
+
+      it 'shows only parties I am hosting if not invited to any' do
         user = create(:user)
         party_1 = create(:party, user: user)
         party_2 = create(:party, user: user)
 
-        expect(user.viewing_parties_invovling_me).to eq([party_1, party_2])
+        expect(user.viewing_parties_involving_me).to eq([party_1, party_2])
       end
 
-      xit 'includes parties I am invited to' do
+      it 'shows only invited-to parties if I am not hosting any' do
+        user = create(:user)
+        party_1 = create(:party)
+        party_2 = create(:party)
+        invitation_1 = create(:invitee, party: party_1, user: user)
+        invitation_2 = create(:invitee, party: party_2, user: user)
 
-      end
-
-      xit 'shows hosted and invited-to parties together' do
-
+        expect(user.viewing_parties_involving_me).to eq([party_1, party_2])
       end
 
       it 'returns empty array if I am not involved with any parties :(' do
         user = create(:user)
-        expect(user.viewing_parties_invovling_me).to eq([])
+        expect(user.viewing_parties_involving_me).to eq([])
       end
     end
   end
