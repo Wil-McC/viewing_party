6a24f1a33a8e11cb3641fe40982ce1686bbc037a
Alex Schwartz <aschwartz@detaso.com>
2021-03-30 15:25:30 -0500

    Added login prereq to movies controller along with show action
    Co-authored-by: Wil McCauley <64031242+Wil-McC@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,14 @@
 class MoviesController < ApplicationController
+  before_action :require_login
+
   def index
     if params[:query] == "top_forty"
       @results = TMDBService.movies
     else
       @results = TMDBService.results(params[:query])
     end
+  end
+
+  def show
   end
 end
