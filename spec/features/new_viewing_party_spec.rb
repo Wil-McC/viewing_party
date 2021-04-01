2e2956cc0e9d4d1501052654bd39626204aeb18d
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-31 19:42:18 -0600

    Add additional tests

--- spec/features/new_viewing_party_spec.rb
+++ spec/features/new_viewing_party_spec.rb
@@ -23,7 +23,28 @@ RSpec.describe 'creating a new viewing party' do
       end
     end
 
-    xit 'displays a form with duration of party' do
+    it "saves a viewing party and displays it on the user's show page" do
+      VCR.use_cassette('rambo_movie_show_page') do
+        user_1 = create(:user)
+
+        login_and_visit_path(user_1)
+        expect(current_path).to eq(new_party_path)
+
+        fill_in 'party[start_time]', with: '20220101\t0315'
+        fill_in 'party[duration]', with: 190
+
+        click_on 'Create Party'
+        expect(current_path).to eq(dashboard_path)
+
+        within('#viewing-party-list') do
+          party_elements = page.all('.viewing-party-card') #does this test anything?
+          expect(page).to have_content('Rambo')
+          expect(page).to have_content('Nobody is invited to this viewing party')
+        end
+      end
+    end
+
+    it "invites friends to the viewing party" do
       VCR.use_cassette('rambo_movie_show_page') do
         user_1 = create(:user)
         user_2 = create(:user)
@@ -36,14 +57,10 @@ RSpec.describe 'creating a new viewing party' do
         friendship_3 = create(:friendship, user: user_1, friend: user_4)
         friendship_4 = create(:friendship, user: user_1, friend: user_5)
 
-        friendship_3 = create(:friendship, user: user_2, friend: user_4)
-        friendship_4 = create(:friendship, user: user_2, friend: user_5)
-
         login_and_visit_path(user_1)
         expect(current_path).to eq(new_party_path)
 
         fill_in 'party[start_time]', with: '20220101\t0315'
-        # fill_in 'party[start_time]', with: '20220101111'
         fill_in 'party[duration]', with: 190
 
         within("#friend-#{friendship_1.friend_id}") do
@@ -55,17 +72,102 @@ RSpec.describe 'creating a new viewing party' do
         end
 
         click_on 'Create Party'
+        expect(current_path).to eq(dashboard_path)
 
-        # TODO what to test now?
+        within('#viewing-party-list') do
+          expect(page).to have_content('Rambo')
+          expect(page).to have_content("#{user_2.email}")
+          expect(page).to have_content("#{user_4.email}")
+          expect(page).to_not have_content("#{user_3.email}")
+          expect(page).to_not have_content("#{user_5.email}")
+        end
+      end
+    end
+
+    it "shows a friend the viewing parties they are invited to" do
+      VCR.use_cassette('rambo_movie_show_page') do
+        user_1 = create(:user)
+        user_2 = create(:user)
+
+        friendship_1 = create(:friendship, user: user_1, friend: user_2)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+        visit movie_path(7555)
+        click_on('Create Viewing Party for Movie')
+        expect(current_path).to eq(new_party_path)
+
+        fill_in 'party[start_time]', with: '20220101\t0315'
+        fill_in 'party[duration]', with: 190
+
+        within("#friend-#{friendship_1.friend_id}") do
+          page.check
+        end
+
+        click_on 'Create Party'
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
+
+        visit dashboard_path
+
+        within('#viewing-party-list') do
+          expect(page).to have_content('Rambo')
+          expect(page).to have_content("Hosted by: #{user_1.email}")
+          expect(page).to have_content("#{user_2.email}")
+        end
+      end
+    end
+
+
+    it "does not show a friend viewing parties they are not invited to" do
+      VCR.use_cassette('rambo_movie_show_page') do
+        user_1 = create(:user)
+        user_2 = create(:user)
+        user_3 = create(:user)
+
+        friendship_1 = create(:friendship, user: user_1, friend: user_2)
+        friendship_2 = create(:friendship, user: user_1, friend: user_3)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+        visit movie_path(7555)
+        click_on('Create Viewing Party for Movie')
+        expect(current_path).to eq(new_party_path)
+
+        fill_in 'party[start_time]', with: '20220101\t0315'
+        fill_in 'party[duration]', with: 190
+
+        within("#friend-#{friendship_1.friend_id}") do
+          page.check
+        end
+
+        click_on 'Create Party'
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_3)
+
+        visit dashboard_path
+
+        within('#viewing-party-list') do
+          expect(page).to have_content('You currently have no viewing parties.')
+          expect(page).to_not have_content("#{user_1.email}")
+          expect(page).to_not have_content("#{user_2.email}")
+        end
       end
     end
   end
 
   describe 'sad path' do
-    xit 'does not allow a user to enter a duration shorter than the movie length' do
-    end
+    it 'does not allow a user to enter a duration shorter than the movie length' do
+      VCR.use_cassette('rambo_movie_show_page') do
+        user_1 = create(:user)
 
-    xit 'does not allow a suer to enter a start date prior to the current date' do
+        login_and_visit_path(user_1)
+        expect(current_path).to eq(new_party_path)
+
+        fill_in 'party[start_time]', with: '20220101\t0315'
+        fill_in 'party[duration]', with: 20
+
+        click_on 'Create Party'
+        expect(current_path).to eq(dashboard_path)
+      end
     end
   end
 
