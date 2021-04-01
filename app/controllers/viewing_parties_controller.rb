aea549b0870ed3e78ea008e47e3d3e2ba8f5451b
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-31 19:41:37 -0600

    Create action

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -8,12 +8,30 @@ class ViewingPartiesController < ApplicationController
 
 
   def create
+    saved_movie = Movie.find_or_create_by(api_id: movie_params[:api_id]) do |movie|
+      movie.name = movie_params[:title]
+    end
+
+    # require "pry"; binding.pry
+    new_party = Party.new(user_id: current_user.id, movie_id: saved_movie.id, start_time: party_params[:start_time], duration: party_params[:duration])
+    new_party.save
+
+
+
+    if params.has_key?(:friend_user_ids)
+      params[:friend_user_ids].each do |id|
+        invited = Invitee.new(user_id: id, party_id: new_party.id)
+        invited.save
+      end
+    end
+
+    redirect_to dashboard_path
   end
 
   private
 
   def movie_info_params
-    @_movie_params ||= params.require(:movie).permit(:api_id, :title, :runtime)
+    params.require(:movie).permit(:api_id, :title, :runtime)
   end
 
   def movie_info
@@ -23,4 +41,14 @@ class ViewingPartiesController < ApplicationController
       runtime: movie_info_params[:runtime]
     })
   end
+
+  def movie_params
+    params.require(:party).require(:movie).permit(:api_id, :title, :runtime)
+  end
+
+  def party_params
+    params.require(:party).permit(:start_time, :duration)
+  end
+
+
 end
